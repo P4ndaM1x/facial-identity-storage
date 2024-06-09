855d957125fe68e6bd417d16168cd51db5f7b2a0
tomekkoziel <tomasz.koziel66@gmail.com>
2024-06-09 13:28:45 +0200

    scanner function and sql file fix

--- init.sql
+++ init.sql
@@ -2,7 +2,13 @@ CREATE EXTENSION IF NOT EXISTS vector;
 
 CREATE TABLE IF NOT EXISTS person (
   id SERIAL PRIMARY KEY,
-  name text UNIQUE,
+  person_id BIGINT UNIQUE NOT NULL,
+  name TEXT UNIQUE NOT NULL,
+  date_of_birth DATE, -- format yyyy-mm-dd
+  place_of_birth TEXT,
+  phone_number TEXT,
+  lib_card_exp_date DATE, -- format yyyy-mm-dd
+  bicycle_card_no INT,
   embedding vector,
   created_at timestamptz DEFAULT now()
 );
